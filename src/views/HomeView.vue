a881ccdc1f9657cff625511b6bb0d383784b9a46
artur.trotskyi <artur.trotskyi@ukr.net>
2025-02-14 10:30:30 +0200

    simple counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,44 @@
+<script setup>
+import {ref, computed} from 'vue';
+
+const count = ref(0);
+
+function increaseCount() {
+  count.value++
+}
+
+function decreaseCount() {
+  count.value--
+}
+
+const oddOrEven = computed(() => {
+  if (count.value % 2 === 0) return 'even';
+  return 'odd';
+})
+</script>
+
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <hr>
+    <div>
+      The counter is: {{ oddOrEven }}
+    </div>
   </div>
 </template>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
